b5a1d03d1198b08e6c04aca8967b41c8eebdc94a
Gaurav Verma <icodelife307@gmail.com>
2023-09-15 12:21:35 +0530

    Updates

--- assets/index-6967e754.js
+++ assets/index-6967e754.js
@@ -109,4 +109,4 @@ Error generating stack: `+o.message+`
   }
 
   }
-`,Kf=({data:e})=>X.jsxs("div",{className:"expensesChartBody",children:[X.jsx(Nh,{}),X.jsx(Yf,{data:e})]});Kf.propTypes={data:Du.checkPropTypes.node};const Am=[{day:"mon",amount:17.45},{day:"tue",amount:34.91},{day:"wed",amount:52.36},{day:"thu",amount:31.07},{day:"fri",amount:23.39},{day:"sat",amount:43.28},{day:"sun",amount:25.48}];function Im(){return X.jsx("div",{children:X.jsx(Kf,{data:Am})})}Yo.createRoot(document.getElementById("root")).render(X.jsx(Rr.StrictMode,{children:X.jsx(Im,{})}));
+`,Kf=({data:e})=>X.jsxs("div",{className:"expensesChartBody",children:[X.jsx(Nh,{}),X.jsx(Yf,{data:e})]});Kf.propTypes={data:Du.checkPropTypes.node};const Am=[{day:"mon",amount:17.45},{day:"tue",amount:34.91},{day:"wed",amount:52.36},{day:"thu",amount:31.07},{day:"fri",amount:23.39},{day:"sat",amount:43.28},{day:"sun",amount:25.48}];function Im(){return X.jsxs("div",{children:["Hello World",X.jsx(Kf,{data:Am})]})}Yo.createRoot(document.getElementById("root")).render(X.jsx(Rr.StrictMode,{children:X.jsx(Im,{})}));
